89739a71aca3a480b03ce7e6559e647c8a930838
moisutsu <moisutsu@gmail.com>
2020-06-08 00:13:02 +0900

    [Add] install.shにpreztoを追加

--- configs/install.sh
+++ configs/install.sh
@@ -11,3 +11,6 @@ curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poet
 
 # cargo
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
