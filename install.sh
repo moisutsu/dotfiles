8ef6b7edd69856c203914467815f70a403b22253
moisutsu <moisutsu@gmail.com>
2020-06-08 22:18:18 +0900

    [Remove] install.shからcargoとPoetryを削除

--- install.sh
+++ install.sh
@@ -23,11 +23,5 @@ chsh -s `which zsh`
 # anyenv
 anyenv install --init
 
-# Poetry
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
-
-# cargo
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
 # prezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
