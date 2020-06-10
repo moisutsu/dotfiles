ddaa11cdeae31aa8fe41315649a2c0c745c8a207
moisutsu <moisutsu@gmail.com>
2020-06-11 00:58:37 +0900

    [Fix] zshがないときにインストールするように修正

--- install-min.sh
+++ install-min.sh
@@ -2,7 +2,7 @@
 
 export HOMEBREW_NO_ENV_FILTERING=1
 
-if command -v zsh 1>/dev/null 2>&1; then
+if !(command -v zsh 1>/dev/null 2>&1); then
     sudo -k
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     if [ ! "$(uname)" = 'Darwin' ]; then
