53e2a2b4c20f91637394a97b34653d5c477f4138
moisutsu <moisutsu@gmail.com>
2020-06-11 16:15:38 +0900

    [Fix] brew用の環境変数の設定場所を修正

--- install-min.sh
+++ install-min.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-export HOMEBREW_NO_ENV_FILTERING=1
-
 if !(command -v zsh 1>/dev/null 2>&1); then
     sudo -k
+    export HOMEBREW_NO_ENV_FILTERING=1
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     if [ ! "$(uname)" = 'Darwin' ]; then
         if [ -e $HOME/.linuxbrew/bin/brew ];then
