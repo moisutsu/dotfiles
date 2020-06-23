a654867e1f3f23ba2d5796e1da57591ade40d844
moisutsu <moisutsu@gmail.com>
2020-06-23 17:01:55 +0900

    [Add] #1 link時に、ファイルがある場合はバックアップを取る

--- link.sh
+++ link.sh
@@ -2,16 +2,26 @@
 
 DOT_DIRECTORY="$HOME/dotfiles"
 SPECIFY_FILES=(Brewfile)
+BACKUP_DIRECTORY="$HOME/.backup/dotfiles"
 
 cd `dirname $0`
+
+mkdir -p $BACKUP_DIRECTORY
 
 for f in .??* ${SPECIFY_FILES[@]}
 do
     [[ $f = ".git" ]] && continue
     [[ $f = ".gitignore" ]] && continue
     [[ $f = ".DS_Store" ]] && continue
+    if [ -e $HOME/$f ] && [ ! -L $HOME/$f ]; then
+        mv $HOME/$f $BACKUP_DIRECTORY
+    fi
     ln -snfv $DOT_DIRECTORY/$f $HOME/$f
 done
+
+if [ -z "$(ls -a $BACKUP_DIRECTORY)" ]; then
+    rmdir $BACKUP_DIRECTORY
+fi
 
 mkdir -p $HOME/.emacs.d
 ln -snfv $DOT_DIRECTORY/emacs/init.el $HOME/.emacs.d/init.el
