9bfe13d33e62a804bcb846744639af6d13ee6278
moisutsu <moisutsu@gmail.com>
2021-01-04 21:05:56 +0900

    Remove unnecessary install starship

--- install-min.sh
+++ install-min.sh
@@ -16,6 +16,3 @@ fi
 
 # zinit
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
-
-# starship
-curl -fsSL https://starship.rs/install.sh | bash
--- install.sh
+++ install.sh
@@ -30,6 +30,3 @@ anyenv install --init
 
 # zinit
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
-
-# starship
-curl -fsSL https://starship.rs/install.sh | bash
