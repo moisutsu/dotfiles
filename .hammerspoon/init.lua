d47e01e75d03bdac4584202cfeb5e63921d4b2d5
moisutsu <moisutsu@gmail.com>
2021-05-11 21:07:55 +0900

    Update hammerspoon setting

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,5 +1,5 @@
 hs.hotkey.bind({"ctrl"}, "t", function()
-    local alacritty = hs.application.find("alacritty")
+    local alacritty = hs.application.get("Alacritty")
     if alacritty == nil then
         hs.application.launchOrFocus("/Applications/Alacritty.app")
     elseif alacritty:isFrontmost() then
